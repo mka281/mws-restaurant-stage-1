3c3f1170cb74a1b6302267a3287cd7c76656b801
mka <mka281@icloud.com>
2018-03-28 20:55:58 +0300

    Handle requests to service worker

--- sw.js
+++ sw.js
@@ -28,3 +28,35 @@ self.addEventListener('install', (event) => {
       })
   );
 });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) { return response }
+
+        // Clone the request. A request is a stream and
+        // can only be consumed once.
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest)
+          .then(response => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response. A response is a stream and
+            // can only be consumed once.
+            const responseClone = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseClone);
+              });
+
+            return response;
+          });
+      })
+  );
+});
